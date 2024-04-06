3148549a927b3f85f7583007238e81641ea6aa82
Karol Krzykowski <karol_krzykowski@wp.pl>
2024-04-06 13:22:02 +0200

    init

---
+++ deploy.php
@@ -0,0 +1,22 @@
+<?php
+namespace Deployer;
+
+require 'recipe/common.php';
+
+// Config
+
+set('repository', '');
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+// Hosts
+
+host('oracle.chipiapps.pl')
+  ->set('remote_user', 'deployer')
+  ->set('deploy_path', '/var/www/test');
+
+// Hooks
+
+after('deploy:failed', 'deploy:unlock');
